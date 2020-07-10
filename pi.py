ccde5d800c36d8238c91f60c10ba3d949f464e0b
Raquel Ruiz <vruizraquel@usp.br>
2020-07-10 17:04:14 -0300

    :o

--- pi.py
+++ pi.py
@@ -1,5 +1,4 @@
 from random import uniform
-
 import matplotlib.pyplot as plt
 
 
@@ -31,6 +30,24 @@ def pi(dic):
     return 4 * n_circle / len(dic.values())
 
 
-z = colors(100000)
-f = plt.scatter(*zip(*z.keys()), c=z.values(), s=0.5)
-plt.show()
+def plot_circle(p):
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.scatter(*zip(*p.keys()), c=p.values(), s=0.5)
+    ax.spines['top'].set_color('none')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['left'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.arrow(0, 0, 1.05, 0, head_width=0.04, head_length=0.05, fc='k', ec='k', lw=0.001)
+    ax.arrow(0, 0, 0, 1.05, head_width=0.04, head_length=0.05, fc='k', ec='k', lw=0.001)
+    ax.set_xlabel('x')
+    ax.set_xticks([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1])
+    ax.xaxis.set_label_coords(0.993, 0.48)
+    ax.set_ylabel('y', rotation=0)
+    ax.set_yticks([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1])
+    ax.yaxis.set_label_coords(0.477, 0.98)
+    ax.tick_params(labelsize=8)
+    plt.show()
+
+
+plot_circle(colors(1000))
