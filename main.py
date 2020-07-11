5c70bc67cfcd64b2c7f49754a568f88998b957ac
Raquel Ruiz <vruizraquel@usp.br>
2020-07-11 01:48:56 -0300

    plots

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from pi import *
 
 if __name__ == '__main__':
-    s = 10000  # number of points
+    p = 10000  # number of points
 
     n = int(input('Quantas vezes? '))
-    pi_list = [pi(colors(s)) for i in range(n)]  # list with n values of pi
+    pi_list = [pi(colors(p)) for i in range(n)]  # list with n values of pi
 
     med = sum(pi_list)/len(pi_list)
     print(f'Média: {med:.5f}')
 
-    plot_points(colors(s))
-    plot_hist(pi_list)
+    plot(colors(p), p, n, pi_list)
--- pi.py
+++ pi.py
@@ -2,14 +2,14 @@ from random import uniform
 import matplotlib.pyplot as plt
 
 
-def colors(n):  # n = Number of points
-    r = 1
+def colors(p):  # p = number of points
+    r = 1  # radius value
     d = {}
 
     color_list = ['#E14E65', '#F98D52', '#EBE051', '#6DBB5B', '#5286DA', '#714EB3']
-    dist = [r*k/len(color_list) if k != 0 else 0 for k in range(len(color_list), -1, -1)]
+    dist = [r * k / len(color_list) if k != 0 else 0 for k in range(len(color_list), -1, -1)]
 
-    for i in range(n):
+    for i in range(p):
         x = uniform(-r, r)  # point value on the x axis
         y = uniform(-r, r)  # point value on the y axis
 
@@ -36,34 +36,42 @@ def pi(dic):
     return 4 * n_circle / len(dic.values())
 
 
-def plot_points(p):
-    fig = plt.figure(figsize=(6.4, 6.4))
-    ax = fig.gca()
-    ax.add_artist(plt.Circle((0, 0), radius=1, lw=0.5, fill=False))
-    ax.add_artist(plt.Rectangle((-1, -1), 2, 2, lw=0.5, fill=False))
-    ax.scatter(*zip(*p.keys()), c=p.values(), s=0.5)
-    ax.spines['top'].set_color('none')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['left'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.arrow(0, 0, 1.05, 0, head_width=0.04, head_length=0.05, fc='k', ec='k', lw=0.001)
-    ax.arrow(0, 0, 0, 1.05, head_width=0.04, head_length=0.05, fc='k', ec='k', lw=0.001)
-    ax.set_xlabel('x')
-    ax.set_xticks([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1])
-    ax.xaxis.set_label_coords(0.993, 0.48)
-    ax.set_ylabel('y', rotation=0)
-    ax.set_yticks([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1])
-    ax.yaxis.set_label_coords(0.477, 0.98)
-    ax.tick_params(labelsize=8)
+def plot(di, p, n, li):
+    """Plots both circle/square example scatter plot and histogram with pi value estimates"""
+    mean = sum(li) / len(li)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12.8, 6.4))  # 1x2 matrix of plots
+
+    # scatter plot for the left plot
+    ax1 = ax[0]
+    ax1.add_artist(plt.Circle((0, 0), radius=1, lw=0.5, fill=False))  # add circle
+    ax1.add_artist(plt.Rectangle((-1, -1), 2, 2, lw=0.5, fill=False))  # add square
+    ax1.scatter(*zip(*di.keys()), c=di.values(), s=0.5)  # scatter plot
+    # creating the cartesian coordinate system
+    ax1.spines['top'].set_color('none')
+    ax1.spines['bottom'].set_position('zero')
+    ax1.spines['left'].set_position('zero')
+    ax1.spines['right'].set_color('none')
+    ax1.arrow(0, 0, 1.05, 0, head_width=0.04, head_length=0.05, fc='k', ec='k', lw=0.001)
+    ax1.arrow(0, 0, 0, 1.05, head_width=0.04, head_length=0.05, fc='k', ec='k', lw=0.001)
+    ax1.set_xlabel('x', size=12)
+    ax1.set_xticks([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1])
+    ax1.xaxis.set_label_coords(0.998, 0.488)
+    ax1.set_ylabel('y', size=12, rotation=0)
+    ax1.set_yticks([-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1])
+    ax1.yaxis.set_label_coords(0.477, 0.98)
+    ax1.tick_params(labelsize=10)
+    # add pi text
+    ax1.text(0.05, 1.02, f'Pi estimado para esse círculo: {pi(di)}', size=11)
+
+    # histogram for the right plot
+    ax2 = ax[1]
+    ax2.hist(li, bins=50, color='#00ffcc')
+    ax2.axvline(mean, color='k', ls='dashed', lw=0.7)  # plots the mean line
+    # add text for the mean line
+    min_ylim, max_ylim = plt.ylim()
+    ax2.text(mean * 1.001, max_ylim * 0.95, f'Média: {mean:.5f}', size=12)
+    # title
+    ax2.set_title(f'Histograma para {n} estimativas de pi\nde {p} pontos cada (ex: plot à esquerda)')
+
     plt.show()
-
-
-def plot_hist(list_pi):
-    med = sum(list_pi) / len(list_pi)
-
-    plt.hist(list_pi, bins=100, color='#00ffcc')
-    plt.axvline(med, color='k', ls='dashed', lw=0.7)
-    # min_ylim, max_ylim = plt.ylim()
-    # plt.text(med * 1.02, max_ylim * 0.9, f'Média: {med:.5f}', size=4)
-    plt.show()
-
